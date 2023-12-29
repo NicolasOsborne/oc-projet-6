b093ec6b42cbcb93fa38c79d9be4531b498fc876
Nicolas Osborne <nho.osborne@gmail.com>
2023-12-29 13:40:30 +0100

    Intégration des éléments "Mode édition" sur la page d'accueil : Bannière en haut de la page + Bouton "modifier"

---
+++ FrontEnd/assets/scripts/modale.js
@@ -0,0 +1,9 @@
+// Si l'utilisateur est connecté
+
+// Afficher le bandeau "Mode édition" au sommet de la page
+
+// Changer "login" par "logout" dans la barre de navigation
+
+// Afficher l'option "modifier" à côté du titre "Mes projets"
+
+// Cacher les boutons pour filtrer les projets par catégorie
